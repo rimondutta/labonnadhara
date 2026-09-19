0fc699812b533a6b6fff2b2d33eb6cb538f99244
Rimon Dutta <darklegend12359@gmail.com>
2026-09-19 15:50:50 +0600

    speed incress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,9 +98,9 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-NWR9G1FPB6"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -68,7 +68,7 @@ export default function FacebookPixel() {
     <>
       <Script
         id="fb-pixel"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             !function(f,b,e,v,n,t,s)
